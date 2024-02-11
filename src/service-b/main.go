5a824612e6c344ed6d1d8045a32a03c2999f5e5e
Mark Day <markday1962@users.noreply.github.com>
2024-02-11 11:59:09 +0000

    Create main.go

---
+++ src/service-b/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("<h1>Welcome to my web server!</h1>"))
+    })
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
